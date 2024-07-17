712e50d43ce64bbd7e37a0ac128cd8589928245f
dmarco-65 <dmarco@e-is.pro>
2024-07-17 09:57:42 +0200

    [KALIUM]: add support for Darwin OSX (MacOS) for libsodium + upgrade jnr-ffi to LST

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-. scripts/version.sh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    . version.sh    # Mac OSX
+else
+. scripts/version.sh  # All OS except Mac OSX
+fi
 
 wget -c https://download.libsodium.org/libsodium/releases/$LIBSODIUM
 tar xzvf $LIBSODIUM
